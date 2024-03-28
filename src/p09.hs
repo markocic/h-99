efac528f8a49bc5e92c56bb8fc188f173e0d8109
markocic <markokocic2000@gmail.com>
2024-03-28 21:20:35 +0100

    add solution to problem 09

---
+++ src/p09.hs
@@ -0,0 +1,10 @@
+-- Problem 09
+-- (**) Pack consecutive duplicates of list elements into sublists.
+
+pack :: (Eq a) => [a] -> [[a]]
+pack x = pack' x []
+
+pack' :: (Eq a) => [a] -> [[a]] -> [[a]]
+pack' [] acc = acc
+pack' (x:xs) acc = pack' b (acc ++ [x:a])
+    where (a, b) = span ( == x) xs
