6849a2c8a8ff228b16f9d0a56ad855ec05989596
markocic <markokocic2000@gmail.com>
2024-03-28 22:34:11 +0100

    add solution to problem 11

---
+++ src/p11.hs
@@ -0,0 +1,21 @@
+-- Problem 11
+-- (*) Modified run-length encoding.
+
+import P09
+data Encoded a = Single a | Multiple Int a
+    deriving Show
+
+
+encodeModified :: (Eq a) => [a] -> [Encoded a]
+
+-- this is meant to piss of my friend :)
+-- hi arsen
+encodeModified xs = [
+        if length x /= 1
+        then
+            Multiple (length x) (head x)
+        else
+            Single (head x)
+    | x <- pack xs 
+    ]
+
